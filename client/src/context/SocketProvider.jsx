0b82950d12bd2fed60f42da7c31e281d86eea820
Shiyam-7 <131951377+Shiyam-7@users.noreply.github.com>
2023-09-28 09:07:12 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://video-call-app-using-react-api.vercel.app/"), []);
+  const socket = useMemo(() => io(""), []);
 
   return (
     <SocketContext.Provider value={socket}>
